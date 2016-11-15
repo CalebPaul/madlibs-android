1a5b428fa5b023930c74829695966cd28b5e044c
Caleb Paul and Satchel Grant <caleb@>
2016-11-15 15:51:35 -0800

    add font and clear fields after submit

--- app/src/main/java/com/mad/libs/madlibs/MainActivity.java
+++ app/src/main/java/com/mad/libs/madlibs/MainActivity.java
@@ -1,16 +1,19 @@
 package com.mad.libs.madlibs;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView mAppNameTextView;
     @Bind(R.id.name) EditText mName;
     @Bind(R.id.noun) EditText mNoun;
     @Bind(R.id.verb) EditText mVerb;
@@ -20,6 +23,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAppNameTextView = (TextView) findViewById(R.id.textView);
+        Typeface fortyTwoFont = Typeface.createFromAsset(getAssets(), "fonts/1942.ttf");
+        mAppNameTextView.setTypeface(fortyTwoFont);
         ButterKnife.bind(this);
 
         mSubmitWordsButton.setOnClickListener(new View.OnClickListener() {
@@ -32,8 +39,10 @@ public class MainActivity extends AppCompatActivity {
                 intent.putExtra("name", name);
                 intent.putExtra("noun", noun);
                 intent.putExtra("verb", verb);
+                mName.setText("");
+                mNoun.setText("");
+                mVerb.setText("");
                 startActivity(intent);
-
             }
         });
     }
