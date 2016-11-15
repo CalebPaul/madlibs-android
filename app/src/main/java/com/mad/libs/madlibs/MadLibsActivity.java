93e2de76b022bccd74101437827e7fe2aa306b41
Caleb Paul and Satchel Grant <caleb@>
2016-11-15 11:46:54 -0800

    add text views to madlibs activity

---
+++ app/src/main/java/com/mad/libs/madlibs/MadLibsActivity.java
@@ -0,0 +1,14 @@
+package com.mad.libs.madlibs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MadLibsActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mad_libs);
+    }
+}
--- app/src/main/java/com/mad/libs/madlibs/MainActivity.java
+++ app/src/main/java/com/mad/libs/madlibs/MainActivity.java
@@ -1,13 +1,39 @@
 package com.mad.libs.madlibs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.name) EditText mName;
+    @Bind(R.id.noun) EditText mNoun;
+    @Bind(R.id.verb) EditText mVerb;
+    @Bind(R.id.submitWords) Button mSubmitWordsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mSubmitWordsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = mName.getText().toString();
+                String noun = mNoun.getText().toString();
+                String verb = mVerb.getText().toString();
+                Intent intent = new Intent(MainActivity.this, MadLibsActivity.class);
+                intent.putExtra("name", name);
+                intent.putExtra("noun", noun);
+                intent.putExtra("verb", verb);
+                startActivity(intent);
+            }
+        });
     }
 }
